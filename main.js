42ec068257c8a3a057c5839878c12127cf793abf
ryana0 <78768691+dafydd69@users.noreply.github.com>
2022-01-30 14:49:39 -0800

    added like new stuff yea

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const string = 'hi'
+let path = document.querySelector('path')
+let pathLength = path.getTotalLength()
+
+path.style.strokeDasharray = pathLength + ' ' + pathLength
+path.style.strokeDashoffset = pathLength
+
+window.addEventListener('scroll', () => {
+    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+    var drawLength = pathLength * scrollPercentage
+    console.log(drawLength)
+    path.style.strokeDashoffset = pathLength - drawLength
+})
