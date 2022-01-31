3d595e2587bbb4b9210d6f2c357b83db34cd72cb
ryana0 <78768691+dafydd69@users.noreply.github.com>
2022-01-30 16:31:55 -0800

    the top part thing is done

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let path = document.querySelector('path')
 let pathLength = path.getTotalLength()
 
 path.style.strokeDasharray = pathLength + ' ' + pathLength
-path.style.strokeDashoffset = pathLength
+path.style.strokeDashoffset = pathLength - 350
 
 window.addEventListener('scroll', () => {
     var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)
