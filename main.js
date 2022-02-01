e05df5892abffa363aae6ef8a30ff1085bd0f0ea
ryana0 <78768691+dafydd69@users.noreply.github.com>
2022-01-31 18:53:22 -0800

    responsive now i hope

--- main.js
+++ main.js
@@ -7,6 +7,5 @@ path.style.strokeDashoffset = pathLength - 350
 window.addEventListener('scroll', () => {
     var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)
     var drawLength = pathLength * scrollPercentage
-    console.log(drawLength)
     path.style.strokeDashoffset = pathLength - drawLength
 })
