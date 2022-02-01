1b4b2ad3c9f5d31db25efb87ee43c3c5e400fa01
ryana0 <78768691+dafydd69@users.noreply.github.com>
2022-01-31 21:32:50 -0800

    finished title thingy

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 const types = document.querySelector('#namePath')
-const typesLength = types.getTotalLength()
+const typesLength = document.querySelector('#namePath').getTotalLength()
 let path = document.querySelector('#cheesePath')
 let pathLength = path.getTotalLength()
 
 path.style.strokeDasharray = pathLength + ' ' + pathLength
 path.style.strokeDashoffset = pathLength - 350
 
+types.style.strokeDasharray = typesLength + ' ' + typesLength
+types.style.strokeDashoffset = typesLength
+
 
 window.addEventListener('scroll', () => {
     var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)
     var drawLength = pathLength * scrollPercentage
     path.style.strokeDashoffset = pathLength - drawLength
+
+    if(window.scrollY >= '3100') {
+        types.classList.add('drawIn')
+    }
 })
+
