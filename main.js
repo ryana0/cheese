276624a9a43252c9e8156b20f6628eddc0f841b1
ryana0 <78768691+dafydd69@users.noreply.github.com>
2022-02-03 22:32:14 -0800

    new things

--- main.js
+++ main.js
@@ -4,19 +4,26 @@ let path = document.querySelector('#cheesePath')
 let pathLength = path.getTotalLength()
 
 path.style.strokeDasharray = pathLength + ' ' + pathLength
-path.style.strokeDashoffset = pathLength - 350
+path.style.strokeDashoffset = pathLength - 400
 
 types.style.strokeDasharray = typesLength + ' ' + typesLength
 types.style.strokeDashoffset = typesLength
 
 
 window.addEventListener('scroll', () => {
-    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) + 0.05
     var drawLength = pathLength * scrollPercentage
-    path.style.strokeDashoffset = pathLength - drawLength
+    path.style.strokeDashoffset = (pathLength - 350) - (drawLength)
 
-    if(window.scrollY >= '3100') {
+    if(scrollPercentage >= 0.50) {
         types.classList.add('drawIn')
+    } else {
+        types.classList.remove('drawIn')
     }
 })
 
+document.querySelectorAll('.cardL').forEach(element => {
+    element.addEventListener('click', () => {
+        element.classList.add('slideIn')
+    })
+})
